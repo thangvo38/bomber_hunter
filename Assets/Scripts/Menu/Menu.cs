5394ea98ff45c723dd11af322f3603de5ac20e56
1512500 <thanhvantran0@gmail.com>
2018-12-30 23:01:31 +0700

    Menu cơ bản, Di chuyển quả bom lên xuống các tùy chọn

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour
+{
+    private Image[] arrImage = new Image[5];
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int index = 0;
+    // Use this for initialization
+    void Start()
+    {
+        arrImage[0] = GameObject.FindGameObjectWithTag("NewGame").GetComponent<Image>();
+        arrImage[1] = GameObject.FindGameObjectWithTag("Load").GetComponent<Image>();
+        arrImage[2] = GameObject.FindGameObjectWithTag("Options").GetComponent<Image>();
+        arrImage[3] = GameObject.FindGameObjectWithTag("About").GetComponent<Image>();
+        arrImage[4] = GameObject.FindGameObjectWithTag("Exit").GetComponent<Image>();
+
+        for (int i = 1; i < 5; i++)
+        {
+            arrImage[i].enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleKeyboard();
+    }
+
+    private void handleKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            index += 1;
+            if (index > 4)
+                index = 0;
+            enableSpriteBoom();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            index -= 1;
+            if (index < 0)
+                index = 4;
+            enableSpriteBoom();
+        }
+    }
+
+    private void enableSpriteBoom()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            arrImage[i].enabled = false;
+        }
+        arrImage[this.index].enabled = true;
+    }
 }
