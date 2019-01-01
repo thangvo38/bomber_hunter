ad9ada663b707a94fcb5cc083c4cd31e21d0b26f
thangvo38 <thangvo8797@gmail.com>
2019-01-01 18:51:59 +0700

    Complete all stages except 6

--- Assets/Scripts/NPCs/SemiFinalBoss.cs
+++ Assets/Scripts/NPCs/SemiFinalBoss.cs
@@ -5,65 +5,63 @@ using UnityEngine.Tilemaps;
 
 public class SemiFinalBoss : UnitStatus {
 
+    public Vector3 startPos = new Vector3(0.5f, 3.5f, 0f);
     GameObject player = null;
-    bool findOtherWay = false;
+    private Rigidbody2D rb2D;
+    bool returnToStart = false;
+    public Vector3 targetPos;
 
     protected override void Awake () {
         base.Awake ();
         player = GameObject.Find ("Player");
         blockTiles = new List<Tilemap>();
+                rb2D = gameObject.AddComponent<Rigidbody2D>();
+                targetPos = player.transform.position;
     }
 
     protected override void Update () {
-        base.Update ();
-
         if (lives <= 0) {
             //Add animation here
         }
 
-        if (isMoving) {
-            return;
+        if (returnToStart && this.transform.position == startPos)
+        {
+            returnToStart = false;
         }
 
-        isMoving = true;
-        MovementControl ();
+        if (!returnToStart && this.transform.position == player.transform.position)
+        {
+            returnToStart = true;
+        }
+        
+        if (!isMoving)
+        {
+            isMoving = true;
+            MovementControl ();
+        }
     }
 
     protected override void MovementControl () {
-
-        Vector3Int newDir;
-        if (!findOtherWay)
-            newDir = Services.ToVectorOne ((Vector3Int.FloorToInt (player.transform.position - this.transform.position)));
-        else {
-            newDir = new Vector3Int(Random.Range(-1, 2),Random.Range(-1, 2), 0);
-            findOtherWay = false;
+        if (returnToStart)
+        {
+            targetPos = startPos;
+        } else {
+            targetPos = player.transform.position;
         }
-        xDir = newDir.x;
-        yDir = newDir.y;
-
-        if (xDir != 0)
-            yDir = 0;
-
-        if (xDir != 0 || yDir != 0) {
-            direction = new Vector2Int (xDir, yDir);
-            Move (xDir, yDir);
-        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        isMoving = false;
     }
 
     protected override void OnCollisionEnter2D (Collision2D other) {
+        Services.IgnoreCollisionByTag(gameObject,other ,Constants.WALL_TAG);
         Services.IgnoreCollisionByTag (this.gameObject, other, Constants.ENEMY_TAG);
         Services.IgnoreCollisionByTag (this.gameObject, other, Constants.DESTRUTABLE_TAG);
         Services.IgnoreCollisionByTag (this.gameObject, other, Constants.BOMB_TAG);
         Services.IgnoreCollisionByTag (this.checkAhead, other, Constants.BOMB_TAG);
 
-        switch (other.gameObject.tag) {
-            case Constants.ENEMY_WALL:
-                stopMoving ();
-                isMoving = false;
-                // direction *= -1;
-                Move (direction.x, direction.y);
-                findOtherWay = true;
-                break;
+        if (other.gameObject.tag == Constants.ENEMY_WALL)
+        {
+             returnToStart = true;
         }
     }
 
